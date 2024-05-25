b76991b4112d823567f8528dcfb739f1ebc057e4
Alexander Chasovsky <alexander.chasovsky@gmail.com>
2024-05-25 12:14:48 +0300

    upd

--- docs/plotly.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $("#includedContent").load("plot.html"); 
-});
-
-
--- docs/scripts/.ipynb_checkpoints/plotly_plots-checkpoint.js
+++ docs/scripts/.ipynb_checkpoints/plotly_plots-checkpoint.js
@@ -2,6 +2,3 @@ $(function(){
   $("#pci_scandinavia").load("img/pci_scandinavia.html"); 
 });
 
-$(function(){
-  $("#pci_scandinavia1").load("img/pci_scandinavia1.html"); 
-});
